3c7a20013fc1c714217c85643aa340a553711e1b
Satish K Oraon <satishkumar@iitbhilai.ac.in>
2022-03-09 10:58:11 +0530

    C1 imported pyweb.io,pandas

---
+++ medical_using_sk_learn.py
@@ -0,0 +1,4 @@
+from pywebio.input import input, FLOAT, NUMBER, TEXT
+from pywebio.output import put_text, put_markdown, put_loading, put_collapse, put_table, toast
+import pandas as pd
+import pywebio
