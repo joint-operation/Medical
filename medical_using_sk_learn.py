d99558676c01f1695c16caf827542b52a2e62c4c
Satish K Oraon <satishkumar@iitbhilai.ac.in>
2022-03-09 15:09:32 +0530

    sym_check function added

--- medical_using_sk_learn.py
+++ medical_using_sk_learn.py
@@ -2,3 +2,9 @@ from pywebio.input import input, FLOAT, NUMBER, TEXT
 from pywebio.output import put_text, put_markdown, put_loading, put_collapse, put_table, toast
 import pandas as pd
 import pywebio
+
+x_symptoms = []
+
+def symp_check(x):
+    if x not in x_symptoms:
+        return 'Try again !!'
